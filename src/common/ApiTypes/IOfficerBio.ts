4e0fe7185e9fa43ee22c5ec9f38a9de7248127b5
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2023-04-30 01:34:27 +0100

    Finshed forces page almost.

--- src/common/ApiTypes/IOfficerBio.ts
+++ src/common/ApiTypes/IOfficerBio.ts
@@ -1,3 +1,6 @@
 export default interface IOfficerBio {
-  bio: string;
+  bio: string | null;
+  contact_details?: {};
+  name: string;
+  rank: string;
 }
--- src/components/ForcesDataArray/ForceModal.tsx
+++ src/components/ForcesDataArray/ForceModal.tsx
@@ -1,23 +1,25 @@
 import IAllForce from "../../common/ApiTypes/IAllForces";
 import Modal from "@mui/material/Modal";
-import { Box, Typography, Grid, TableRow, TableCell } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Grid,
+  TableRow,
+  TableCell,
+  Button,
+} from "@mui/material";
 import { useQuery } from "react-query";
 import ApiServiceProvider from "../../utils/ApiServiceProvider";
 import IPoliceService from "../../common/ApiTypes/IPoliceService";
 import ICrimeReport from "../../common/ApiTypes/ICrimeReport";
 import IOfficerBio from "../../common/ApiTypes/IOfficerBio";
-import MUIDataTable, {
-  MUIDataTableColumnDef,
-  TableBodyCell,
-  TableBodyRow,
-} from "mui-datatables";
+import MUIDataTable from "mui-datatables";
 const style = {
   position: "absolute",
   top: "50%",
   minHeight: "50vh",
   maxHeight: "80vh",
-  maxWidth: "100vh",
-  minWidth: "100vh",
+  minWidth: "150vh",
   left: "50%",
   transform: "translate(-50%, -50%)",
   bgcolor: "background.paper",
@@ -26,11 +28,11 @@ const style = {
   alignItems: "center",
   boxShadow: 24,
   p: 4,
-  padding: 2,
   overflowY: "auto",
 };
 interface IModalAddOnFuncProps {
   reports: [ICrimeReport[], IPoliceService, IOfficerBio[]];
+  closeModal: () => void;
 }
 interface IForceModalProps {
   force: IAllForce;
@@ -40,7 +42,6 @@ const getDate = (dateString: string) => {
   const dateArray: string[] = dateString.split("-");
   const year: number = parseInt(dateArray[0]);
   const month: number = parseInt(dateArray[1]);
-
   const date: Date = new Date(Date.UTC(year, month - 1));
   const timestamp: number = Math.floor(date.getTime() / 1000);
   return timestamp;
@@ -50,32 +51,49 @@ const ModalAddonFunc: React.FC<IModalAddOnFuncProps> = (
 ) => {
   const {
     reports: [crimeReport, policeService, officerBio],
+    closeModal,
   } = props;
   const sortedCrimeReports = crimeReport.sort((a, b) => {
     const [aTimeStamp, bTimeStamp] = [getDate(a.month), getDate(b.month)];
     return aTimeStamp - bTimeStamp;
   });
+  const muiData = officerBio
+    .filter(
+      (officer, index, self) =>
+        index === self.findIndex((o) => o.name === officer.name)
+    )
+    .sort((a, b) => {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
   return (
     <Grid
       container
       justifyContent="center"
       alignItems="center"
       direction="column"
-      spacing={1}
+      spacing={2}
     >
       <Grid item>
-        <Typography fontSize={35} variant="subtitle2">
+        <Typography fontSize={45} variant="subtitle2">
           {policeService.name}
         </Typography>
       </Grid>
       <Grid item>
-        <Typography fontSize={25} variant="subtitle2">
+        <Typography fontSize={35} variant="subtitle2">
           {policeService.id}
         </Typography>
       </Grid>
       {policeService.description && (
         <Grid item>
-          <Typography fontSize={15} variant="subtitle2">
+          <Typography fontSize={19} variant="subtitle2">
             <div
               dangerouslySetInnerHTML={{ __html: policeService.description }}
             />
@@ -83,10 +101,105 @@ const ModalAddonFunc: React.FC<IModalAddOnFuncProps> = (
         </Grid>
       )}
       <Grid item sx={{ mb: 2 }}>
-        <Typography fontSize={15} variant="subtitle2">
+        <Typography fontSize={19} variant="subtitle2">
           Phone number: {policeService.telephone}
         </Typography>
       </Grid>
+      <Grid item>
+        <Button variant="outlined" onClick={closeModal}>
+          Close
+        </Button>
+      </Grid>
+      {officerBio.length > 0 &&
+        (officerBio[0].bio || officerBio[0].contact_details) && (
+          <Grid item width="100%">
+            <Grid
+              container
+              justifyContent="center"
+              alignItems="center"
+              direction="column"
+              textAlign="center"
+              spacing={1}
+            >
+              <Grid item width="100%">
+                <MUIDataTable
+                  title="Officers"
+                  columns={Object.keys(officerBio[0]).map((x) => {
+                    return {
+                      name: x,
+                      label: x,
+                      ...(!(x === "name" || x === "rank") && {
+                        options: { display: "excluded" },
+                      }),
+                    };
+                  })}
+                  data={muiData}
+                  options={{
+                    selectableRows: "none",
+                    filter: false,
+                    expandableRows: true,
+                    renderExpandableRow: (rowData, rowMeta) => {
+                      const x = muiData[rowMeta.dataIndex];
+                      const colSpan = rowData.length + 1;
+                      return (
+                        <TableRow>
+                          <TableCell colSpan={colSpan}>
+                            <Grid
+                              width="100%"
+                              container
+                              justifyContent="center"
+                              alignItems="center"
+                              direction="column"
+                              padding={1}
+                              textAlign="center"
+                            >
+                              <div>
+                                {x.name && (
+                                  <Grid item>
+                                    <Typography
+                                      fontSize={30}
+                                      variant="subtitle2"
+                                    >
+                                      Name: {x.name}
+                                    </Typography>
+                                  </Grid>
+                                )}
+                                {x.rank && (
+                                  <Grid item>
+                                    <Typography
+                                      fontSize={25}
+                                      variant="subtitle2"
+                                    >
+                                      Rank: {x.rank}
+                                    </Typography>
+                                  </Grid>
+                                )}
+                                {x.bio && (
+                                  <Grid item>
+                                    <Typography
+                                      fontSize={15}
+                                      variant="subtitle2"
+                                    >
+                                      <div
+                                        dangerouslySetInnerHTML={{
+                                          __html: x.bio,
+                                        }}
+                                      />
+                                    </Typography>
+                                  </Grid>
+                                )}
+                              </div>
+                            </Grid>
+                          </TableCell>
+                        </TableRow>
+                      );
+                    },
+                  }}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+        )}
       {crimeReport.length > 0 && (
         <Grid item width="100%">
           <MUIDataTable
@@ -94,14 +207,14 @@ const ModalAddonFunc: React.FC<IModalAddOnFuncProps> = (
               return {
                 name: x,
                 label: x,
-                ...(!(x === "category" || x === "month" || x === "id") && {
+                ...(!(x === "category" || x === "id") && {
                   options: {
-                    display: "excluded",
+                    display: x === "month" ? "true" : "excluded",
                     ...(x === "month" && {
                       sortCompare: (order: string) => (a: any, b: any) => {
                         const [aTimeStamp, bTimeStamp] = [
-                          getDate(a.month),
-                          getDate(b.month),
+                          getDate(a.data),
+                          getDate(b.data),
                         ];
                         if (order === "asc") {
                           return aTimeStamp - bTimeStamp;
@@ -121,7 +234,6 @@ const ModalAddonFunc: React.FC<IModalAddOnFuncProps> = (
               filter: false,
               expandableRows: true,
               renderExpandableRow: (rowData, rowMeta) => {
-                console.log(rowData);
                 const crime = sortedCrimeReports[rowMeta.dataIndex];
                 const colSpan = rowData.length + 1;
                 return (
@@ -134,7 +246,6 @@ const ModalAddonFunc: React.FC<IModalAddOnFuncProps> = (
                         alignItems="center"
                         direction="column"
                         padding={1}
-                        spacing={1}
                         textAlign="center"
                       >
                         <Grid item width="100%">
@@ -146,6 +257,29 @@ const ModalAddonFunc: React.FC<IModalAddOnFuncProps> = (
                               {crime.month}
                             </Typography>
                           </Grid>
+                          <Grid item>
+                            <Typography
+                              fontSize={18}
+                              variant="subtitle2"
+                              sx={{ mb: 3 }}
+                            >
+                              {crime.id}
+                            </Typography>
+                          </Grid>
+                          {crime.context && (
+                            <Grid item>
+                              <Typography fontSize={18} variant="subtitle2">
+                                {crime.context}
+                              </Typography>
+                            </Grid>
+                          )}
+                          {crime.outcome_status?.category && (
+                            <Grid item>
+                              <Typography fontSize={18} variant="subtitle2">
+                                {crime.outcome_status.category}
+                              </Typography>
+                            </Grid>
+                          )}
                         </Grid>
                       </Grid>
                     </TableCell>
@@ -197,14 +331,25 @@ export const ForceModal: React.FC<IForceModalProps> = (
           padding={4}
         >
           {isLoading || !data ? (
-            <Grid item>
+            <Grid
+              item
+              sx={{
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                position: "absolute",
+                top: "50%",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
               <Typography fontSize={35} variant="subtitle2">
                 Loading...
               </Typography>
             </Grid>
           ) : (
             <Grid item width="100%">
-              <ModalAddonFunc reports={data} />
+              <ModalAddonFunc reports={data} closeModal={closeModal} />
             </Grid>
           )}
         </Grid>
